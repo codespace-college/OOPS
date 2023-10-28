1029a05c1ec7c41dc9aa980db3d08405c77b2e62
codespace-college <145272503+codespace-college@users.noreply.github.com>
2023-10-28 12:23:03 +0000

    main

--- AYUSH PRAGNESH MODI/2303031057006/practical3.java
+++ AYUSH PRAGNESH MODI/2303031057006/practical3.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class practical3 {
 
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in); System.out.print("Enter a number: ");
-int number = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
 
-if (number % 2 == 0) { System.out.println(number + " is even.");
-}
+        if (number % 2 == 0) { 
+            System.out.println(number + " is even.");
+        }
 
-else {
-System.out.println(number + " is odd.");
-}
+        else {
+            System.out.println(number + " is odd.");
+        }
 
-}
+    }
 
 }
