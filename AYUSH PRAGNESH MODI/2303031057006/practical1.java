2b1ce0f6e28ee9ac3aac112d6593489324815d6e
codespace-college <145272503+codespace-college@users.noreply.github.com>
2023-10-28 12:16:32 +0000

    main

--- AYUSH PRAGNESH MODI/2303031057006/practical1.java
+++ AYUSH PRAGNESH MODI/2303031057006/practical1.java
@@ -1,4 +1,4 @@
-public class helloworld
+public class practical1
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
--- AYUSH PRAGNESH MODI/2303031057006/practical3.java
+++ AYUSH PRAGNESH MODI/2303031057006/practical3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class oddEven {
+public class practical3 {
 
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in); System.out.print("Enter a number: ");
